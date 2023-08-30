234a999ad6417c064ee4353dd7a0709be7e5e488
Scott Houseman <scott.houseman@takealot.com>
2023-08-30 08:33:00 +0200

    Fix unit tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,9 +5,9 @@ def test_cli__valid_input_arg_given():
     """
     Given: The cli is invoked with a `--input` argument
     When: The `--input` file path is valid
-    Then: The command should return an exit code of 0
+    Then: The command should return an exit code of 0.
     """
-    from league_ranker import cli
+    from ranker.main import cli
 
     runner = CliRunner()
     result = runner.invoke(cli, ["--input", "data/data.in"])
@@ -18,9 +18,9 @@ def test_cli__invalid_input_arg_given():
     """
     Given: The cli is invoked with a `--input` argument
     When: The `--input` file path is invalid (the file does not exist)
-    Then: The command should return an exit code of 2
+    Then: The command should return an exit code of 2.
     """
-    from league_ranker import cli
+    from ranker.main import cli
 
     runner = CliRunner()
     result = runner.invoke(cli, ["--input", "data/foo.in"])
@@ -32,9 +32,9 @@ def test_cli__valid_stdin_given():
     """
     Given: The cli is invoked with no argument
     When: Valid data is passed as input through STDIN
-    Then: The command should return an exit code of 0
+    Then: The command should return an exit code of 0.
     """
-    from league_ranker import cli
+    from ranker.main import cli
 
     runner = CliRunner()
     result = runner.invoke(cli, input="Lions 1, FC Awesome 1\n")
